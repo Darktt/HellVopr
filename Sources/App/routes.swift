e822d6464562864891baf9e06602e1a454b1a490
Eden Li <eden@realtime.tw>
2018-10-10 16:52:50 +0800

    Update routes.

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -5,35 +5,46 @@ import Foundation
 public func routes(_ router: Router) throws
 {
     // Basic "It works" example
-    router.get { req in
+    router.get {
+        req in
+        
         return "It works!"
     }
     
     // Basic "Hello, world!" example
-    router.get("hello") { req in
+    router.get("hello") {
+        
+        req in
+        
         return "Hello, world!"
     }
-    
-    let dateClosure: (Request) -> String = {
-        
-        _ in
-        
-        let timeZone = TimeZone.CST
-        
-        let dateFormatter = DateFormatter()
-        dateFormatter.timeZone = timeZone
-        dateFormatter.dateFormat = "YYYY-MM-dd HH:mm:SS aa"
-        
-        let date = Date()
-        
-        return dateFormatter.string(from: date)
-    }
-    
-    router.get("date", use: dateClosure)
 
     // Example of configuring a controller
     let todoController = TodoController()
     router.get("todos", use: todoController.index)
     router.post("todos", use: todoController.create)
     router.delete("todos", Todo.parameter, use: todoController.delete)
+    
+    // Date route.
+    let dateClosure: (Request) -> String = {
+        
+        _ in
+        
+        let timeZone = TimeZone(abbreviation: "GMT+0800")
+        
+        guard let _timeZone = timeZone else {
+            
+            return "Time zone error."
+        }
+        
+        let date = Date()
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.timeZone = _timeZone
+        dateFormatter.dateFormat = "YYYY-MM-dd HH:mm:SS aa"
+        
+        return dateFormatter.string(from: date)
+    }
+    
+    router.get("date", use: dateClosure)
 }
